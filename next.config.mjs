668dfb341949cd7312a184f54db637e5f0185f17
fuadfakhruz <mfuadfakhruzzaki@students.undip.ac.id>
2024-07-03 01:22:46 +0700

    komit lagi

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   output: "export",
   distDir: "dist",
+  basePath: "",
+  assetPrefix: "",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
